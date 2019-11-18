ba44ba6f86a38ab43eea97c5231df339ca277145
Valtteri <valtterijniemela@gmail.com>
2019-11-18 15:31:44 +0200

    bug fixes and stuff
    Team logo template, waypoints, fixed scaling, fixed colliders, fixed player getting stuck to walls, fixed player not moving randomly.

---
+++ Assets/Scripts/TeamLogo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamLogo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject logoPanel;
+    void Start()
+    {
+        logoPanel.SetActive(true);
+        StartCoroutine("teamLogo");
+    }
+
+    IEnumerator teamLogo()
+    {
+        yield return new WaitForSeconds(3f);
+        logoPanel.SetActive(false);
+    }
+}
