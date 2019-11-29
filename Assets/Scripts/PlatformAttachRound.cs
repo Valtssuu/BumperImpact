447dc6e3bcf39a3442e25dfd1e609bd969ed671d
Valtteri <n6niva00@students.oamk.fi>
2019-11-29 15:07:13 +0200

    level 8, 9 and 10 work and bossenemy hpbar disappears

---
+++ Assets/Scripts/PlatformAttachRound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttachRound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+   
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject == player || other.gameObject.tag == "Enemy")
+        {
+
+            player.transform.SetParent(this.transform, true);
+            other.transform.SetParent(this.transform, true);
+
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject == player || other.gameObject.tag == "Enemy")
+        {
+            player.transform.parent = null;
+            other.transform.parent = null;
+        }
+    }
+}
